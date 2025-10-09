b4e9e997517c734a716adcce08d6d3cbd6469235
ubaiddevpk <obaidullah.developer@gmail.com>
2025-10-09 13:48:47 +0500

    env updated

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,7 +5,7 @@ import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", createUser);   
+router.post("/", protect,createUser);   
 router.post("/login", loginUser);       // POST /api/users/login
 // router.get("/profile", protect, getProfile); // GET /api/users/profile
 
